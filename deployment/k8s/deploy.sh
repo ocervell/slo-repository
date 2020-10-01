a4d5822749825cc6a03f7f0563fe24384621f0db
Olivier Cervello <ocervello@google.com>
2020-10-01 18:32:50 +0200

    Update SLOs

--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #!/bin/bash
-kubectl create configmap slos --from-file gunicorn-opentelemetry-poc/ --from-file slo-generator/ -o yaml --dry-run=client | kubectl apply -f -
+kubectl create configmap slos --from-file ../../slos/ -o yaml --dry-run=client | kubectl apply -f -
 kubectl create configmap config --from-file error_budget_policy.yaml -o yaml --dry-run=client | kubectl apply -f -
 kubectl apply -f app.yaml
