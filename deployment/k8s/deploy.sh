65327fdbb4debbd6f3442ccb547f0add9ee29a59
Olivier Cervello <ocervello@google.com>
2021-03-18 11:01:05 +0100

    Add support for v2

--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -13,5 +13,4 @@
 # limitations under the License.
 #!/bin/bash
 kubectl create configmap slos --from-file ../../slos/ -o yaml --dry-run=client | kubectl apply -f -
-kubectl create configmap config --from-file error_budget_policy.yaml -o yaml --dry-run=client | kubectl apply -f -
-kubectl apply -f app.yaml
+kubectl create configmap config --from-file config.yaml -o yaml --dry-run=client | kubectl apply -f -
